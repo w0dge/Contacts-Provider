692c9a53973de67b2df3103a4ca838f03e9d2c61
Gleb Bokun <wodgeonixias@mail.ru>
2020-08-01 16:56:44 +0300

    Initial commit

---
+++ app/src/main/java/wodge/programming/contactsprovider/MainActivity.kt
@@ -0,0 +1,50 @@
+package wodge.programming.contactsprovider
+
+import android.os.Bundle
+import android.provider.ContactsContract
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+private const val TAG = "MainActivity"
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        fab.setOnClickListener { view ->
+            Log.d(TAG, "fab onClick: starts")
+            val projection = arrayOf(ContactsContract.Contacts.DISPLAY_NAME_PRIMARY)
+
+            val cursor = contentResolver.query(ContactsContract.Contacts.CONTENT_URI,
+            projection,
+            null,
+            null,
+            ContactsContract.Contacts.DISPLAY_NAME_PRIMARY)
+
+            Log.d(TAG, "fab onClick: ends")
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
